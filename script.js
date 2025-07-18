d450082b50161f3179e975d2349a42e408c9ae93
clemintheweb <148592398+clemintheweb@users.noreply.github.com>
2025-07-18 10:55:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const map = L.map('map').setView([45.5017, -73.5673], 13);
 
-  L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-    minZoom: 0,
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    ext: 'png'
-  }).addTo(map);
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  minZoom: 0,
+  maxZoom: 20,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
+  subdomains: 'abcd'
+}).addTo(map);
+    
     
     L.Control.geocoder({
     defaultMarkGeocode: true,
